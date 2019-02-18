bfb59d2b224f3eba5577918665dc3ccd24916df2
Yohta Ito <y0hta335@gmail.com>
2019-02-19 03:07:07 +0900

    Make Navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-  #devise_for :users
+  devise_for :users
   #devise_for :users, :controllers => {
   #  sessions: 'users/sessions', 
   #  registrations: 'users/registrations'
   #}
   root to: 'home#index'
   get '/', to: 'home#index'
-  get '/users/sign_in', to: 'home#index'
-  get '/users/sign_up', to: 'home#index'
+  #get '/users/sign_in', to: 'home#index'
+  #get '/users/sign_up', to: 'home#index'
   get '/terms', to: 'home#index'
-  post '/users', to: 'users/registrations#create'
+  #post '/users', to: 'users/registrations#create'
   get '/dashboard', to: 'dashboard#index'
 end
