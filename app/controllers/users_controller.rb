78203214ab76c311ab36eecf7c44d3211820f17b
Yohta Ito <y0hta335@gmail.com>
2019-03-06 19:12:08 +0900

    Modified Gemfile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @@user = @user
-    @transactions = Transaction.where(user_id: @user.id).page(params[:page]).per(3)
+    @transactions = Transaction.where(user_id: @user.id).reverse_order.page(params[:page]).per(3)
     impressionist(@user)
   end
 
