8adf94c1d4f0ac0deb0edccb381abe15055eac0f
Yohta Ito <y0hta335@gmail.com>
2019-03-25 06:05:08 +0900

    Delete package-lock.json

--- config/application.rb
+++ config/application.rb
@@ -9,12 +9,12 @@ Bundler.require(*Rails.groups)
 # Dotenv
 Dotenv::Railtie.load
 
+config.assets.initialize_on_precompile = false
 module V2Portfolio
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
